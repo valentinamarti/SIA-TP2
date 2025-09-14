322e4e30e4da139ad89e5e258bb01fe005d5df2c
ivanOdz <76888044+ivanOdz@users.noreply.github.com>
2025-09-14 16:33:41 -0300

    mutating childs instead of parents, added gen reached and time spent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Dict, Any, List
 import numpy as np
+import time
 
 from src.crossover_methods import one_point_crossover, uniform_crossover
 from src.generations_methods import replace_population_traditional, replace_population_young_bias
@@ -35,26 +36,30 @@ def run_ga(image: np.ndarray,
     while gen < GENERATION_AMOUNT and (
         target_error is None or population[0].error is None or population[0].error > target_error
     ):
-        new_population: List[Individual] = []
-
         # selections
         parents = select_individuals(selection_method, population, population_size)
 
         # crossover
+        children = []
         for i in range(0, len(parents), 2):
             p1, p2 = parents[i], parents[i + 1]
             if np.random.rand() < 0.8:  # crossover probability
                 if crossover == "one_point":
                     c1, c2 = one_point_crossover(p1, p2)
-                elif crossover == "uniform":
+                else:  # crossover == "uniform"
                     c1, c2 = uniform_crossover(p1, p2)
             else:
-                c1, c2 = p1.copy(), p2.copy()  # sin crossover, se copian tal cual
+                c1, c2 = p1.copy(), p2.copy()  # sin crossover
+            children.extend([c1, c2])
 
-        # mutate individuals
-        for ind in parents:
-            mutated = mutate_individual(mutation_method, ind, size, prob=0.2, structural=mutate_structure)
+        # mutate children
+        new_population = []
+        for child in children:
+            mutated = mutate_individual(mutation_method, child, size, prob=0.2, structural=mutate_structure)
             new_population.append(mutated)
+
+        # Evaluar fitness de los hijos antes del reemplazo
+        fitness_evaluator.evaluate_population(new_population)
 
         # new generations
         if replacement_method == "traditional":
@@ -70,22 +75,30 @@ def run_ga(image: np.ndarray,
 
     best = population[0]
     save_rendered(best, size,filename = "results/output_rgb.png",)
-    print(f"Fitness: {best.fitness}\nPolígonos: {len(best.polygons)}")
+    print(f"Fitness: {best.fitness}\nPolígonos: {len(best.polygons)}\nGen: {gen}")
     return {"best": best}
 
 
 if __name__ == "__main__":
-    target_img = load_image("images/argentina.png", size=(128,128))
+    target_img = load_image("images/blue_square.png", size=(128,128))
+
+    start = time.time()
     result = run_ga(target_img,
                     max_polygons=40,
                     population_size=40,
                     mutate_structure=True,
                     mutation_method="uniform_multi_gen",
-                    selection_method="tournament",
-                    crossover="one_point",
+                    selection_method="boltzmann",
+                    crossover="uniform_crossover",
                     replacement_method="traditional",
-                    polygon_sides=3,
-                    target_error=0.0001)
+                    polygon_sides=4,
+                    target_error=0.01)
+
+    end = time.time()  # ⏱️ fin
+    elapsed = end - start
+
+    print(f"Tiempo total de ejecución: {elapsed:.2f} segundos")
+
 
 
 
